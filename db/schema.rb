59e97da83a5b8853004bcfbf962d78319751d42c
yuliatokaryk <julia4099@gmail.com>
2023-05-16 12:50:17 +0300

    books added

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_16_091305) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_16_092214) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "author", default: "unknown", null: false
+    t.text "description"
+    t.datetime "finish", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_16_091305) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "books", "users"
 end
