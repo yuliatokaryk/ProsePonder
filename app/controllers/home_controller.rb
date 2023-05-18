acf854d3fe8b11323ca2f8fa8eebd8a624ae1c74
yuliatokaryk <julia4099@gmail.com>
2023-05-18 17:38:18 +0300

    add before_action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,4 @@
 class HomeController < ApplicationController
   def index
-    if current_user
-      redirect_to user_path(current_user.id)
-    else
-      redirect_to new_user_session_path
-    end
   end
 end
