930a9d645f43b16cab6da4713e6b3da257cc89a4
yuliatokaryk <julia4099@gmail.com>
2023-05-23 12:18:20 +0300

    book show styles added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root "home#index"
   devise_for :users
-  resources :books, except: [:new]
+
+  resources :books, except: [:new] do
+    resources :notes, only: [:create]
+  end
+
   resources :authors, except: [:new]
-  resources :notes
+  resources :notes, only: [:show]
   resources :user_books
 end
