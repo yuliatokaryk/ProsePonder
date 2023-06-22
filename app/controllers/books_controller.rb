6087a6b9af4733bc98565bdebd00e2c80e216064
yuliatokaryk <julia4099@gmail.com>
2023-06-22 19:26:59 +0300

    Fix add book page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,40 @@
 class BooksController < ApplicationController
   before_action :set_book, only: %i[show]
 
+  def index
+    @books = Book.all
+    @authors = Author.all
+  end
+
   def show
     @note = Note.new
     @user_notes = Note.where(user: current_user, book: @book)
+  end
+
+  def new
+    @book = Book.new
+    @authors = Author.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      UserBook.create(user_id: current_user.id, book_id: @book.id)
+      redirect_to root_path
+    else
+      @authors = Author.all
+      render 'new'
+    end
   end
 
   private
 
   def set_book
     @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :author_id, author_attributes: [:name])
   end
 end
--- app/controllers/user_books_controller.rb
+++ app/controllers/user_books_controller.rb
@@ -1,49 +1,24 @@
 class UserBooksController < ApplicationController
-  before_action :set_user_book, only: %i[show]
-
   def new
     @user_book = UserBook.new
     @books = Book.all
-    @authors = Author.all
-    @book = Book.new
   end
 
   def create
     @user_book = UserBook.new(user_book_params)
     @user_book.user = current_user
-    @user_book.state = "reading"
-
-    if params[:user_book][:book_id].present?
-      @user_book.book_id = params[:user_book][:book_id]
-    elsif params[:user_book][:title].present?
-      if params[:user_book][:author_id].present?
-        author = params[:user_book][:author_id]
-      elsif params[:user_book][:new_author_name].present?
-        author = params[:user_book][:new_author_name]
-      else
-        author = nil
-      end
-      author = Author.find_or_create_by(name: params[:user_book][:new_author_name])
-      book = Book.find_or_create_by(title: params[:user_book][:title], author: author)
-      @user_book.book = book
-    end
 
     if @user_book.save
       redirect_to root_path
     else
       @books = Book.all
-      @authors = Author.all
       render 'new'
     end
   end
 
   private
 
-  def set_user_book
-    @user_book = UserBook.find(params[:id])
-  end
-
   def user_book_params
-    params.require(:user_book).permit(:book_id, :title, :author_id, :new_author_name)
+    params.require(:user_book).permit(:book_id)
   end
 end
